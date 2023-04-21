cb459aa06b46b31780e3cc1818d1231ec935d59b
oluwatoyinakinola <110613887+oluwatoyinakinola@users.noreply.github.com>
2023-04-21 12:02:20 -0700

    Add files via upload

---
+++ Score.py
@@ -0,0 +1,19 @@
+print("Enter score of user: ")
+x = int(input())
+
+
+#print("Not allowed, if score is greater than 100 or less than 0 ")
+if x >= 69 and x <= 100:
+    print("A")
+elif x >=59 and x <= 70:    
+    print("B")
+elif x >=49 and x <= 60:    
+    print("C")
+elif x >= 39 and x <= 50:
+    print("D")
+elif x >= 29 and x <= 40:
+    print("E")    
+elif x >= 0 and x <= 30:
+    print("F")
+else:
+    print("Not Allowed!!")    
